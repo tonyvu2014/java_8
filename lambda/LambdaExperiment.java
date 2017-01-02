146c1f80408026ac9a93ca07f568f736cc386e96
Tony Vu <tonyvu2008@gmail.com>
2017-01-02 20:18:46 +0800

    Call default method

--- lambda/LambdaExperiment.java
+++ lambda/LambdaExperiment.java
@@ -5,15 +5,15 @@ import lambda.Computation;
 
 public class LambdaExperiment {
 	public static void main(String[] args) {
-	    Worker roadWorker = () -> {System.out.println("Building road");};
-	    roadWorker.doWork();
+		Worker roadWorker = () -> {System.out.println("Building road");};
+		roadWorker.doWork();
 		
 		int result = doComputation(10, -5, (x, y) -> x+y);
 		System.out.println("Result: " + String.valueOf(result));
 		
-    }
+	}
 	
 	private static int doComputation(int x, int y, Computation computation) {
-		return computation.compute(x, y);
+		return computation.magnify(computation.compute(x, y));
 	} 
 }
